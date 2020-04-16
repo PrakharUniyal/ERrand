af6e72ee7351bc08634c969cfd2ae098dc03a504
Prakhar Uniyal <b18128@students.iitmandi.ac.in>
2020-04-17 00:25:10 +0530

    Add react frontend boilerplate code

---
+++ grm-app/client/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+// components
+import BookList from './components/BookList';
+
+class App extends Component {
+  render() {
+    return (
+        <div id="main">
+          <h1>Ninja's Reading List</h1>
+          <BookList />
+          <BookList />
+      </div>
+    );
+  }
+}
+
+export default App;
